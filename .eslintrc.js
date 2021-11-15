ee665471f84e5685368ed0fd90b191c8b0641e17
Yanzhijie <17310920903@163.com>
2021-11-15 10:11:26 +0800

    docs(初始化项目环境): 增加esLint  pretter  git cz 约束

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,8 +10,17 @@ module.exports = {
   parserOptions: {
     parser: 'babel-eslint'
   },
+  /*
+    线上：production 需要检测  --》 npm run build --> console 移除
+    线下：devlopment 不需要   --》 npm run serve  --> console 输出数据
+    error: 错误 2  
+    warn : 警告 1
+    off: 关闭   0
+  */
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'quotes': 'off',
+    'space-before-function-paren':'off'
   }
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
@@ -9,10 +9,14 @@
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
 
+// 写代码的时候 保存的时候自动将代码格式化（eslint 规范）
 export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  created() {
+    console.log('12321')
   }
 }
 </script>
